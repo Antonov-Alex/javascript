5c8d36e656a3bf643cef8cfcde505a9aa2762032
Antonov-Alex <antonov.mob@gmail.com>
2018-10-06 19:01:49 +0300

    11

---
+++ homework/chernovik/chernovik m11/task1/index.js
@@ -0,0 +1,48 @@
+
+
+
+'use strict';
+
+//-------------- "Task 1/11"-----------------------
+
+//console.log(Handlebars);
+/*
+  К pen уже подключен Handlebars.
+  
+  Создайте шаблон списка указаного во вкладке HTML.
+  
+  Отрендерите список в DOM по данным из массива listItems.
+*/
+
+const listItems = [
+  { name: 'item 1', count: 2 },
+  { name: 'item 2', count: 4 },
+  { name: 'item 3', count: 12 },
+  { name: 'item 4', count: 29 },
+];
+
+const list = document.querySelector('.list');
+const source = document.querySelector('#list-items').innerHTML.trim();
+const template = Handlebars.compile(source);
+const markup = listItems.reduce((acc, item) => acc + template(item), '');
+list.insertAdjacentHTML('afterbegin', markup);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ homework/chernovik/chernovik m11/test/index.js
@@ -0,0 +1,43 @@
+
+
+
+'use strict';
+
+//-------------- "TEST"-----------------------
+
+const str = `The quick brown fox jumps over the lazy dog`;
+const patternA = /quick/;
+const patternB = /qwerty/;
+
+console.log (
+  `str.search(patternA) : ${str.search(patternA)}`
+); // 4
+
+console.log (
+  `str.search(patternB) : ${str.search(patternB)}`
+); // -1
+
+const b = /\w+\s*/ig
+const string = 'huy vam llllojyfrt';
+ console.log(`
+ ${string.match(b)} 
+ `)
+//huy ,vam ,llllojyfrt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
